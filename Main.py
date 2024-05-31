f0fdb2d3addd33bfacd4c74239d2a2c1a4e6e992
Spntax <varbiro99@gmail.com>
2024-05-31 21:01:12 +0200

    THing

--- Main.py
+++ Main.py
@@ -1,1 +1,14 @@
+import tkinter
+import textract
+import aspose.words as aw
+main_window = tkinter.Tk()
 print("hehe haha")
+
+template_path = "Base_document.doc"
+output_path = 'out/Output_fun.doc'
+
+doc = aw.Document(template_path)
+doc.save(output_path)
+#text = textract.process("Base_document.doc")
+
+# main_window.mainloop()
