8ae08789ccef18c57a5faf794c2628cd287da2e3
Spntax <varbiro99@gmail.com>
2024-06-01 19:09:31 +0200

    Updated DocumentData class

--- DocData.py
+++ DocData.py
@@ -1,14 +1,36 @@
+from docx import Document
+import docxedit
 class DocumentData:
-    name = ""
-    address = ""
-    p_number = ""
-    notes = ""
-    callsign = ""
-    type_data = ""
-    modell = ""
-    description = ""
-    addons = ""
-    diagnosis = ""
-    year = ""
-    month = ""
-    day = ""
+    def __init__(self):
+        self.name = "Masztur Bálint"
+        self.address = "9700 Szombathely Szopok boré utca. 69/B"
+        self.p_number = "0630-911/4200"
+        self.notes = "Anyjádat xxxd"
+        self.callsign = "az ott"
+        self.type_data = "Mekláren"
+        self.modell = "gyors"
+        self.description = "nincs"
+        self.addons = "az összes"
+        self.diagnosis = "rosz"
+        self.year = "2001"
+        self.month = "09"
+        self.day = "11"
+
+    def UpdateData(self,doc):
+#Iterating trough the document's tables cuz bitch ass docxedit can't replace strings in tables when parsing the whole doc
+        for table in doc.tables:
+            for row in table.rows:
+                for cell in row.cells:
+                    docxedit.replace_string(cell,"{{NAME}}",self.name)
+                    docxedit.replace_string(cell,"{{ADDRESS}}",self.address)
+                    docxedit.replace_string(cell,"{{P_NUMBER}}",self.p_number)
+                    docxedit.replace_string(cell,"{{NOTES}}",self.notes)
+                    docxedit.replace_string(cell,"{{CALLSIGN}}",self.callsign)
+                    docxedit.replace_string(cell,"{{TYPE}}",self.type_data)
+                    docxedit.replace_string(cell,"{{MODELL}}",self.modell)
+                    docxedit.replace_string(cell,"{{DESCRIPTION}}",self.description)
+                    docxedit.replace_string(cell,"{{ADDONS}}",self.addons)
+                    docxedit.replace_string(cell,"{{DIAGNOSIS}}",self.diagnosis)
+                    docxedit.replace_string(cell,"{{YEAR}}",self.year)
+                    docxedit.replace_string(cell,"{{MONTH}}",self.month)
+                    docxedit.replace_string(cell,"{{DAY}}",self.day)
--- Main.py
+++ Main.py
@@ -13,27 +13,11 @@ output_path = 'out/Output_fun.docx'
 
 doc = Document(template_path)
 
-documentData = DocData.DocumentData
+documentData = DocData.DocumentData()
 # Global variables ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
-#Iterating trough the document's tables cuz bitch ass docxedit can't replace strings in tables when parsing the whole doc
 def SaveDocument():
-    for table in doc.tables:
-        for row in table.rows:
-            for cell in row.cells:
-                docxedit.replace_string(cell,"{{NAME}}",documentData.name)
-                docxedit.replace_string(cell,"{{ADDRESS}}",documentData.address)
-                docxedit.replace_string(cell,"{{P_NUMBER}}",documentData.p_number)
-                docxedit.replace_string(cell,"{{NOTES}}",documentData.notes)
-                docxedit.replace_string(cell,"{{CALLSIGN}}",documentData.callsign)
-                docxedit.replace_string(cell,"{{TYPE}}",documentData.type_data)
-                docxedit.replace_string(cell,"{{MODELL}}",documentData.modell)
-                docxedit.replace_string(cell,"{{DESCRIPTION}}",documentData.description)
-                docxedit.replace_string(cell,"{{ADDONS}}",documentData.addons)
-                docxedit.replace_string(cell,"{{DIAGNOSIS}}",documentData.diagnosis)
-                docxedit.replace_string(cell,"{{YEAR}}",documentData.year)
-                docxedit.replace_string(cell,"{{MONTH}}",documentData.month)
-                docxedit.replace_string(cell,"{{DAY}}",documentData.day)
+    documentData.UpdateData(doc)
     doc.save(output_path)
 
 
