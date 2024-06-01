2c2dd64e3a5efd620aac2a6073a1e17d64dedceb
Spntax <varbiro99@gmail.com>
2024-06-01 17:30:11 +0200

    Library setup

--- Main.py
+++ Main.py
@@ -1,14 +1,18 @@
 import tkinter
-import textract
-import aspose.words as aw
+from docx import Document
+import docxedit
+
 main_window = tkinter.Tk()
 print("hehe haha")
 
-template_path = "Base_document.doc"
-output_path = 'out/Output_fun.doc'
+template_path = "Base_document.docx"
+output_path = 'out/Output_fun.docx'
 
-doc = aw.Document(template_path)
+doc = Document(template_path)
+for table in doc.tables:
+    for row in table.rows:
+        for cell in row.cells:
+            docxedit.replace_string(cell,"{{NAME}}","Masztur Bálint")
 doc.save(output_path)
-#text = textract.process("Base_document.doc")
 
 # main_window.mainloop()
